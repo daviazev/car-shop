efda5ef41dfa4aa280e7efc16e8fd61c9162e9b1
daviazevdo <azvd.davi@gmail.com>
2023-02-13 20:24:44 -0300

    criação da classe CarService

---
+++ src/Services/CarService.ts
@@ -0,0 +1,21 @@
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
+import Car from '../Domains/Car';
+
+class CarService {
+  private createCarDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car({ ...car });
+    }
+
+    return null;
+  }
+
+  public async createCarService(car: ICar) {
+    const carODM = new CarODM();
+    const newCar = await carODM.createCar(car);
+    return this.createCarDomain(newCar);
+  }
+}
+
+export default CarService;
